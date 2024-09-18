86528e3b8f5ce77fee6b0d568d56106e8fce23bd
Jacob Ball <jacobtball05@gmail.com>
2024-09-18 12:27:32 -0500

    Added line notes. Fixed bug where it would crash upon passing bottom barrier. Simplified some of the rendering process.

--- wormMain.cpp
+++ wormMain.cpp
@@ -7,7 +7,7 @@
 #include<random>
 
 using namespace std;
-int res_x = 15, res_y = 14; //caps at around 89, 50 (depending on the termanal window size)
+int res_x = 15, res_y = 15; //caps at around 89, 50 (depending on the termanal window size)
 int head_x = 5, head_y = 5, length = 1;
 float pkey;
 bool alive = true;
@@ -26,34 +26,33 @@ int random(int min, int max) {
 */
 void renderScreen() {
 	system("cls");
-	cout << "  'wasd' to move\n";
+	cout << "  'wasd' to move";
 	for (int i = 0; i < res_y; ++i) {
-
-		for (int p = 0; p < res_x; ++p) {
-			if ((food_x == p) and (food_y == i)) {
-				cout << "<>";
-			}
-			else if (screen[i * res_x + p] == length) {
-				if ((pkey == 'w') or (pkey == 's')) {
-					cout << "oo";
+		if (i > 0) {
+			for (int p = 0; p < res_x; ++p) {
+				int pixelValue = screen[i * res_x + p];
+				if ((food_x == p) and (food_y == i)) {
+					cout << "<>";
 				}
-				else {
-					cout << "8 ";
+				else if (pixelValue == length) {
+					if ((pkey == 'w') or (pkey == 's')) {
+						cout << "oo";
+					}
+					else {
+						cout << "8 ";
+					}
 				}
-			}
-			else if (screen[i * res_x + p] > 0) {
-				cout << "  ";
-			}
-			else if (p == 0) {
-				cout << "  ";
-			}
-			else if (screen[i * res_x + p] == 0) {
-				cout << "[]";
+				else if ((pixelValue > 0) or (p == 0)) {
+					cout << "  ";
+				}
+				else if (screen[i * res_x + p] == 0) {
+					cout << "[]";
+				}
 			}
 		}
 		cout << endl;
 	}
-	cout << "  things: " << length-1;
+	cout << "  things: " << length - 1;
 }
 
 /*
@@ -96,24 +95,24 @@ void testMovements() {
 *Updates values on the screen vector by subtracting values greater than zero by 1, and then plotting the position of the head with the value of the length
 */
 void graphPoints() {
-	for (int k = 0; k < screen.size(); k++) {
+	for (int k = 0; k < screen.size(); k++) { //iterate through whole screen, simulate movements
 		if (screen[k] > 0) {
 			screen[k] = screen[k] - 1;
 		}
 	}
 	int yr = round(head_y), xr = round(head_x);
-	if ((screen[(yr)*res_x + xr] > 0) and (pkey)) {
+	if ((yr < 1) or (xr < 1) or (yr > res_y - 1) or (xr > res_x - 1)) {
 		alive = false;
 	}
-	if (((xr > 0 and yr > 0) and (xr < res_x and yr < res_y))) {
-		screen[(yr) * res_x + xr] = length;
+	else if ((screen[(yr)*res_x + xr] > 0) and (pkey)) {
+		alive = false;
 	}
 	else {
-		alive = false;
+		screen[(yr)*res_x + xr] = length;
 	}
-	if ((head_x == food_x) and (head_y == food_y)) {
-		length++;
-		food_x = random(1, res_x - 1);
+	if ((head_x == food_x) and (head_y == food_y)) { //eating procedure
+		length++; //add to worm size
+		food_x = random(1, res_x - 1); //set new position of food
 		food_y = random(1, res_y - 1);
 	}
 }
